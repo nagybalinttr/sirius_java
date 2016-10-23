ce2662f8b017aabd1564331ba6393f1de93783f5
Nagy Balint <nagy.balint.tr@gmail.com>
2016-10-23 17:31:34 +0200

    2016. 10. 20. Gyakorlat

---
+++ src/Szokoev/Szokoev.java
@@ -0,0 +1,19 @@
+package Szokoev;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Szokoev {
+	private static Scanner sc;
+	public static void main(String[] args) {
+		int ev = 0;
+		sc = new Scanner(System.in);
+		System.out.println("Adj meg egy evet es megmondom neked, hogy ay szokoev-e :D");
+		ev = sc.nextInt();
+		
+		if((ev % 4 == 0 && ev % 400 == 0) || (ev % 4 == 0))
+			System.out.println("Igen");
+		else
+			System.out.println("Nem");		
+	}
+}
