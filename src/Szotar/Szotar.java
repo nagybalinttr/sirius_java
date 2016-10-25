3f5d83098c6b88e76149c2326cf125aae35416d5
Balint Nagy <nagy.balint.tr@gmail.com>
2016-10-25 16:33:42 -0700

    ciklus break added

--- src/Szotar/Szotar.java
+++ src/Szotar/Szotar.java
@@ -9,15 +9,14 @@ public class Szotar {
 		Boolean talalat = false;
 		int tomb_hossza = 0;
 		int i = 0;
-		int talalat_helye = 0;
 		String szo = "";
 		
 		String szavak_magyar[] = {"Sajt", "Kenyer", "Sor", "Viz", "Toll", "Tenger", "Utca", "Ter", "Vonat", "Repulo", "Kutya", "Macska", "Lo", "Hal", "Asztal", "Lampa", "Telefon", "Auto", "Kerekpar", "Televizio", "Szamitogep", "Konyv", "Munka", "Feladat"};
 		String szavak_angol[] = {"Cheese", "Bread", "Beer", "Water", "Pen", "Sea", "Street", "Square", "Train", "Aeroplane", "Dog", "Cat", "Horse", "Fish", "Desk", "Lamp", "Phone", "Car", "Bicycle", "Television", "Computer", "Book", "Job", "Task"};
 		
 		tomb_hossza = szavak_magyar.length; //a .lenght adja meg hany elemu a tombunk
-		System.out.println("Adj meg egy magyar szot es megmondom az angol megefelelojet");
 		System.out.println("Jelenleg " + tomb_hossza + " darab szo talalhato az adatbazisban.");
+		System.out.println("Adj meg egy magyar szot es megmondom az angol megefelelojet");
 		szo = sc.nextLine();
 		
 		for(i=0; i < tomb_hossza; i++)
@@ -25,12 +24,12 @@ public class Szotar {
 			if(szo.equals(szavak_magyar[i]))
 			{
 				talalat = true;
-				talalat_helye = i;
+				break; //azert van ra szokseg mert a for ciklus vegig menne a tombon es akkor a talalatkor ervenyes i erteket egy segedvaltozoba el kellene tarolni
 			}
 		}
 		if(talalat) //mivel true -ra vizsgalunk ezert nem kell neki feltetelt adni
 		{
-			System.out.println(szavak_angol[talalat_helye]);
+			System.out.println(szavak_angol[i]);
 		}
 		else
 		{
