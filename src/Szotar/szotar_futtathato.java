a5432625f7b3e238696373ddebf355229029cf77
Balint Nagy <nagy.balint.tr@gmail.com>
2016-10-26 16:35:49 -0700

    futtathato jar added

---
+++ src/Szotar/szotar_futtathato.java
@@ -0,0 +1,43 @@
+package Szotar;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class szotar_futtathato {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					szotar_futtathato window = new szotar_futtathato();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public szotar_futtathato() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
