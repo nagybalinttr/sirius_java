c438cc2837ac9e842a561688cd020cc733b37fad
Nagy Balint <nagy.balint.tr@gmail.com>
2016-12-06 11:40:11 +0100

    some changeses

--- src/Feladatsor/Feladat_1.java
+++ src/Feladatsor/Feladat_1.java
@@ -5,7 +5,8 @@ public class Feladat_1 {
 	public static void main(String[] args) {
 		int a; int b; //bemeneti értékek
 		double q =0; int m = 0; //számolt értékek
-		Scanner sc = new Scanner(System.in); // itt példányos System.out.println("Adj meg két egész számot!");tom a Scanner tipusu sc objektumomat a System.in adja meg hogy a console bol varom az ertekolvasast
+		Scanner sc = new Scanner(System.in); // itt peldanyositom a Scanner tipusu sc objektumomat a System.in adja meg hogy a console bol varom az ertekolvasast
+		System.out.println("Adj meg két egész számot!");
 		a = sc.nextInt();
 		b = sc.nextInt();
 		do{
